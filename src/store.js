ad19bfc96e16777dfb0d35979c2427194d2c64cc
Kirill Tsonev <kirill.tsonev.work@gmail.com>
2022-10-20 18:23:12 +0300

    Redux training

---
+++ src/store.js
@@ -0,0 +1,18 @@
+import { createStore, compose, applyMiddleware } from "redux";
+import rootReducer from "./reducer"
+import { print1, print2, print3 } from './exampleAddons/middleware'
+
+const middlewareEnhancer = applyMiddleware(print1, print2, print3)
+
+const store = createStore(rootReducer, middlewareEnhancer)
+
+export default store
+
+
+function exampleMiddleware(storeAPI) {
+    return function wrapDispatch(next) {
+        return function handleAction(action) {
+            return next(action)
+        }
+    }
+}
