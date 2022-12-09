e4c00e7d5eb284750f52fd583406a3cd1a7fe477
Kirill Tsonev <kirill.tsonev.work@gmail.com>
2022-12-09 19:20:50 +0200

    TodoListItem

--- src/store.js
+++ src/store.js
@@ -4,7 +4,7 @@ import { print1, print2, print3 } from './exampleAddons/middleware'
 
 const middlewareEnhancer = applyMiddleware(print1, print2, print3)
 
-const store = createStore(rootReducer, middlewareEnhancer)
-// const store = createStore(rootReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
+// const store = createStore(rootReducer, middlewareEnhancer)
+const store = createStore(rootReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
 
 export default store
